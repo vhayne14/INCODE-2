9c632b764b25c53533c224956b2f7fc8657e6e8d
vhayne14 <vhayne14@gmail.com>
2021-08-21 20:26:02 +1000

    Console terminal: Okay

--- form.js
+++ form.js
@@ -34,25 +34,85 @@ const form = document.querySelector("#contact")
 
 const validEmail = /^[A-Za-z0-9\._\-]+@[A-Za-z]+[A-Za-z0-9\-]+[A-Za-z]+(\.[A-Za-z0-9\-]+)+$/i;
 const validName = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 \.'\-]+$/;
+const valNumber = /^[0-9]{10,10}$/;   // number should consist of 10 digits e.g. 0412345678
+const validMsg = /^.{0,200}$/; // message is optional and can handle any character with a limit of 200 characters
 
 form.setAttribute = ("novalidate", true);
 
 form.onsubmit = submitForm;
 
+
 function submitForm(e){
+  
   e.preventDefault();
   // modal.style.display = "block";
-
+  // console.log("hello");
   // let test = validName.test(lastName.value);
   // console.log(test);
 
-  console.log("hello");
-  // console.log(inputValidator(validName, firstName)); console.log(inputValidator(validName, lastName));
-  // console.log(inputValidator(validEmail, email));
-  
 
   
-}
+    if (
+      inputValidator(validName, firstName)==true &&
+      inputValidator(validName, lastName)==true &&
+      inputValidator(valNumber, telephone)==true &&
+      inputValidator(validEmail, email)==true &&
+      inputValidator(validMsg, message)==true
+
+    ) {
+      modal.style.display = "block";
+      console.log("FirstName: "+inputValidator(validName, firstName)); 
+      console.log("LastName: "+inputValidator(validName, lastName));
+      console.log("Telephone: ")+inputValidator(valNumber, telephone);
+      console.log("Email: "+inputValidator(validEmail, email));
+      console.log("Message: "+inputValidator(validMsg, message));
+      
+    } 
+    else if(
+      inputValidator(validName, firstName)==true &&
+      inputValidator(validName, lastName)==true &&
+      inputValidator(valNumber, telephone)==false &&
+      inputValidator(validEmail, email)==true &&
+      inputValidator(validMsg, message)==true
+    ){
+      console.log("Please double check your telephone number. It must consist of only numbers with a length of 10\ne.g. 0412345678");
+    }
+    else if(
+      inputValidator(validName, firstName)==false ||
+      inputValidator(validName, lastName)==false &&
+      inputValidator(validEmail, email)==true &&
+      inputValidator(validMsg, message)==true
+    ){
+      console.log("Please check your Nom or Prenom.\nPlease don't include special characters");
+    }
+    else if(
+      inputValidator(validName, firstName)==true &&
+      inputValidator(validName, lastName)==true &&
+      inputValidator(validEmail, email)==false&&
+      inputValidator(validMsg, message)==true
+    ){
+      console.log("Please check your email address and try again");
+    }
+    else if(
+      inputValidator(validName, firstName)==false &&
+      inputValidator(validName, lastName)==true &&
+      inputValidator(validEmail, email)==false&&
+      inputValidator(validMsg, message)==true
+    ){
+      console.log("Please check your nom. Please don't include special characters");
+      console.log("Please check your email address and try again");
+    }
+    else if(
+      inputValidator(validName, firstName)==true &&
+      inputValidator(validName, lastName)==false &&
+      inputValidator(validEmail, email)==false&&
+      inputValidator(validMsg, message)==true
+    ){
+      console.log("Please check your prenom. Please don't include special characters");
+      console.log("Please check your email address and try again");
+    }
+      
+    }
 
 function inputValidator(regex, input){
   return regex.test(input.value);
@@ -65,16 +125,15 @@ function inputValidator(regex, input){
 // var popUpClose = document.querySelector(#popUpClose);
 
 // When the user clicks the button, open the modal 
-btn.onclick = function(e) {
-  e.preventDefault();
-  modal.style.display = "block";
+// btn.onclick = function(e) {
+// //   console.log("hello");
+//   e.preventDefault();
+//   modal.style.display = "block";
 
-
-
-  // document.body.style.overflow = "hidden"; // ADD THIS LINE
+// // document.body.style.overflow = "hidden"; // ADD THIS LINE
 // //   document.body.style.height = "100%"; // ADD THIS LINE
   
-}
+// }
 
 // When the user clicks on <span> (x), close the modal
 // span.onclick = function() {
@@ -94,7 +153,6 @@ popUpClose.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
-
     // document.body.style.overflow = "scroll"; // ADD THIS LINE
     // document.body.style.height = "auto"; // ADD THIS LINE
   }
