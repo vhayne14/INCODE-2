71f7f7b5bbeddd88d2c7e20c44c4262eb9ce909c
vhayne14 <vhayne14@gmail.com>
2021-08-21 19:40:07 +1000

    Implemented regex, console terminal logs not working

--- form.js
+++ form.js
@@ -7,6 +7,60 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+
+
+
+
+//Nom Input
+const lastName = document.querySelector("#nom")
+
+//Prenom Input
+const firstName = document.querySelector("#prenom")
+
+//Telephone Input
+const telephone = document.querySelector("#telephone")
+
+//Address Input
+const email = document.querySelector("#email")
+
+//Message Input
+const message = document.querySelector("#message")
+
+//Actual Form
+const form = document.querySelector("#contact")
+
+
+//Regex
+
+const validEmail = /^[A-Za-z0-9\._\-]+@[A-Za-z]+[A-Za-z0-9\-]+[A-Za-z]+(\.[A-Za-z0-9\-]+)+$/i;
+const validName = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9 \.'\-]+$/;
+
+form.setAttribute = ("novalidate", true);
+
+form.onsubmit = submitForm;
+
+function submitForm(e){
+  e.preventDefault();
+  // modal.style.display = "block";
+
+  // let test = validName.test(lastName.value);
+  // console.log(test);
+
+  console.log("hello");
+  // console.log(inputValidator(validName, firstName)); console.log(inputValidator(validName, lastName));
+  // console.log(inputValidator(validEmail, email));
+  
+
+  
+}
+
+function inputValidator(regex, input){
+  return regex.test(input.value);
+}
+
+
+
+
 // Get pop up button id to close the modal
 // var popUpClose = document.querySelector(#popUpClose);
 
@@ -15,15 +69,17 @@ btn.onclick = function(e) {
   e.preventDefault();
   modal.style.display = "block";
 
+
+
   // document.body.style.overflow = "hidden"; // ADD THIS LINE
 // //   document.body.style.height = "100%"; // ADD THIS LINE
   
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks on popUpClose, close the modal
 popUpClose.onclick = function() {
